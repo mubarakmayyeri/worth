385589737401a704e946b358f3c0205e511ea5ea
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-06-11 14:36:46 +0530

    template for prediction page setup

--- salary_prediction/views.py
+++ salary_prediction/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def prediction(request):
+    if request.method == 'POST':
+        domain = request.POST['domain']
+        age = request.POST['age']
+        print(domain, age)
+        
+    return render(request, 'prediction.html')
--- worth/urls.py
+++ worth/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('salary_prediction/', include('salary_prediction.urls')),
 ]
 
 if settings.DEBUG:
