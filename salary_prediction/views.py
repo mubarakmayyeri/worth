430752f41d16c1758623aa21259204759dc0dfcd
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-06-14 21:48:56 +0530

    corrected the form in prediction.html

--- salary_prediction/views.py
+++ salary_prediction/views.py
@@ -7,6 +7,17 @@ def prediction(request):
     if request.method == 'POST':
         domain = request.POST['domain']
         age = request.POST['age']
-        print(domain, age)
+        designation = request.POST['designation']
+        cs_bg = request.POST['cs_bg']
+        experience = request.POST['experience']
+        degree = request.POST['degree']
+        avg_time = request.POST['avg_time']
+        holidays = request.POST['holidays']
+        week_back = request.POST['week_back']
+        graduate = request.POST['graduate']
+        skills = request.POST.getlist('skills')
+
+        print(domain, age, designation, cs_bg, experience, degree, avg_time, holidays, week_back, graduate)
+        print('Skills', skills)
         
     return render(request, 'prediction.html')
